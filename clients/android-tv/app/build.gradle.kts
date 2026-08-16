738dab29619caad895ad875691f76d9bd183a044
frittcat <137531366+frittcat@users.noreply.github.com>
2026-08-16 19:15:32 +0200

    Bump signed Android TV baseline to 30006

--- clients/android-tv/app/build.gradle.kts
+++ clients/android-tv/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "tv.familystream.client"
         minSdk = 23
         targetSdk = 36
-        versionCode = 30005
-        versionName = "0.3.0-dev.1"
+        versionCode = 30006
+        versionName = "0.3.0-dev.2"
         buildConfigField("String", "DEFAULT_SERVER_URL", "\"http://10.0.2.2:8080\"")
         buildConfigField(
             "String",
