f3b76d4aa453c447c3e947d6c365c9a349eaa02c
frittcat <137531366+frittcat@users.noreply.github.com>
2026-08-16 20:28:33 +0200

    Bump Android TV for fullscreen player UX

--- clients/android-tv/app/build.gradle.kts
+++ clients/android-tv/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "tv.familystream.client"
         minSdk = 23
         targetSdk = 36
-        versionCode = 30008
-        versionName = "0.3.0-dev.4"
+        versionCode = 30009
+        versionName = "0.3.0-dev.5"
         buildConfigField("String", "DEFAULT_SERVER_URL", "\"http://10.0.2.2:8080\"")
         buildConfigField(
             "String",
